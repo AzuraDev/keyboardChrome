fd8d84cae9bf4391806fdf8994b204aaa991a1ed
AzuraDev <azuradev@gmail.com>
2014-10-09 18:40:13 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
-javascript: document.onkeydown = flvsScript;
-function flvsScript(e){
+javascript: document.onkeydown = safeWeb;
+function safeWeb(e){
   if(e.keyCode == 49){
   window.open('http://www.google.com');
 }
