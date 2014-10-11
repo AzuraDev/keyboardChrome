94521d5ef99b0d6ebe03ae7ae1390563e1d5dd62
Azura <azuradev@gmail.com>
2014-10-11 00:10:11 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -7,5 +7,9 @@ function flvsScript(e){
   }else if(e.keyCode == 50){
     var css = document.getElementsByTagName('link');
     css[0].href = "none.css";
+  }else if(e.keyCode == 51){
+    window.open('http://www.google.com');
+  }else if(e.keyCode == 52){
+    window.location.reload(true);
   }
 }
