ea6182d60dc9f5e9bc8743f859b508b1d7e185c8
Azura <azuradev@gmail.com>
2014-10-10 23:51:54 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,11 @@
-javascript: document.onkeydown = safeWeb;
-function safeWeb(e){
+javascript:
+var x = 0;
+document.onkeydown = flvsScript;
+function flvsScript(e){
   if(e.keyCode == 49){
-  window.open('http://www.google.com');
-}
+    window.location.href = 'http://www.flvs.com';
+  }else if(e.keyCode == 50){
+    var css = document.getElementsByTagName('link');
+    css[0].href = "none.css";
+  }
 }
